e8d2b1929483b36106468a79992b37e9e5ac70fb
F003 <stafilip.bop@gmail.com>
2022-07-27 19:51:34 +0200

    ex_1.13 working

--- ANSI_C_exercises/ex_1.13.c
+++ ANSI_C_exercises/ex_1.13.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#define IN 1
+#define OUT 0
+#define MAX_LENGHT 27
+/* I assumed that sigle word takes no more that 27 characters.
+That is the lenght of the longest word used in the work of Shakespeare:
+honorificabilitudinitatibus. */
 
 int main()
 {
-	int nw[32], lw;
+	int c, state, wl;
+	int nw[MAX_LENGHT]; //number of words of certain lenght
+	
+	state = OUT;
+	for (int i = 0; i < MAX_LENGHT; ++i)
+		nw[i] = 0;
+	while((c = getchar()) != EOF) {
+		if (c != ' ' && c != '\t' && c != '\n' && c != ',' && c != '.' && c != ':' && c != ';') { //I think there is a better way to put it
+			if (state)
+				wl = 0;
+			++wl;
+		}
+		else {
+			//I assumed that input will not begin with whitespaces or punctuation
+			nw[wl-1] = nw[wl-1] + 1;
+			wl = 0;
+			state = OUT;
+		}
+			
+	}
+	for (int i = 0; i < MAX_LENGHT; ++i){
+		printf("%2d ", i + 1);
+		for (int j = 0; j < nw[i]; ++j)
+			putchar('|');
+		putchar('\n');
+	}
 
-	lw 
+}
