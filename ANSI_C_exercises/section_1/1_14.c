f6b62ff3d81e1cf40ec3ccc296fc6ef1b1771485
F003 <f003@github.com>
2022-07-29 19:27:07 +0200

    Basic solution to ex 1.14

--- ANSI_C_exercises/section_1/1_14.c
+++ ANSI_C_exercises/section_1/1_14.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#define ASCII_START 9 //first ASCII character that I am considering as usefull is horizontal tab (ASCII 9)
+#define ASCII_END 126 //the last usefull ASCII char that I acknowledged is tilde (ASCII 126)
+#define NUMBER_OF_CHARACTERS 117 //126 - 9 = 117 characters that the app will be counting
 
-int main(){
+int main()
+{
+	int c, nc[NUMBER_OF_CHARACTERS];
 
-	int c;
-	while ((c = getchar()) != EOF) {
+	for (int i = 0; i < (NUMBER_OF_CHARACTERS); ++i)
+		nc[i] = 0;
+	
+	while ((c = getchar()) != EOF)
+		if (c > ASCII_START || c < ASCII_END)
+			++nc[c-ASCII_START];
 
+	for (int i = 0; i < NUMBER_OF_CHARACTERS; ++i){
+		printf("%3d ", i + ASCII_START);
+		for (int j = 0; j < nc[i]; ++j)
+			putchar('|');
+		putchar('\n');
+	}
+
+}
