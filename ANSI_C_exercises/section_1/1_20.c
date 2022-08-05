4ec25c45289ddea6931370f8d6fff3ba9bdcc8ca
F003 <81417629+F003@users.noreply.github.com>
2022-08-05 21:15:49 +0200

    working 1.20 AKA detab

---
+++ ANSI_C_exercises/section_1/1_20.c
@@ -0,0 +1,25 @@
+// program should be called detab, but I've chosen to stay with consistent naming 
+#include <stdio.h>
+#define MAXLENGHT 1000000
+#define SPACES_IN_TAB 4
+
+int main()
+{
+	int j, i = 0;
+	char c, input[MAXLENGHT];
+	while (i < MAXLENGHT - 1 && (c=getchar()) != EOF){
+		input[i] = c;
+		if (c == '\t')
+			for (j = 0;	j < SPACES_IN_TAB; ++j) {
+				input[i] = ' ';
+				++i;
+			}
+		else
+			++i;
+	}
+	input[i] = '\0';
+
+	printf("%s", input);
+	
+	return 0;
+}
