72fe3936ea730f8e4e69c920bda0b2015eae23bb
F003 <81417629+F003@users.noreply.github.com>
2022-08-09 17:57:06 +0200

    working 1.22 hopefully working. That was hardest exercise so far. I am proud of myself

--- ANSI_C_exercises/section_1/1_22.c
+++ ANSI_C_exercises/section_1/1_22.c
@@ -12,16 +12,57 @@ int main()
 	char line[MAXLENGHT];
 	int len;
 
-	while ((len = get_line(line, MAXLENGHT)) > 0)
+	while ((len = get_line(line, MAXLENGHT)) > 0) {
+			lbr_pointer = 0;
 			linebreaker(line, len);
+	}
 
 	return 0;
 }
 
 void linebreaker(char line[], int len)
-
-
+{
+	/* skipping whitespaces on beggining of lines */ 
+	while ((line[lbr_pointer] == ' ' || line[lbr_pointer] == '\t') && lbr_pointer < len-1)
+		++lbr_pointer;
+	/* string is printeble in single line */
+	if (len - lbr_pointer <= MAXPRINTSIZE + 1) {
 		
+		while (lbr_pointer < len-1) {
+			putchar(line[lbr_pointer]);
+			++lbr_pointer;
+		}
+		putchar('\n');
+	}
+
+	/* string is not printable in a single line */
+	else {
+		int i = lbr_pointer + MAXPRINTSIZE;
+		while (i > lbr_pointer && line[i] != '\t' && line[i] != ' ') /* searching for viable breaking point */
+			--i;
+
+		/* viable breaking point have been found */
+		if (i != lbr_pointer) {
+			while (lbr_pointer < i) {
+				putchar(line[lbr_pointer]);
+				++lbr_pointer;
+			}
+		}
+
+		/* breaking point not found - printing '...' instead of additional characters */
+		else {
+			while (lbr_pointer < i + MAXPRINTSIZE - 3) { 
+				putchar(line[lbr_pointer]);
+				++lbr_pointer;
+			}
+			while (line[lbr_pointer] != ' ' && line[lbr_pointer] != '\t' && line[lbr_pointer] != '\n')
+				++lbr_pointer;
+			printf("...");
+		}
+		putchar('\n');
+		linebreaker(line, len); /* using the same function for the rest of a string */
+	}
+}
 
 int get_line(char line[], int max)
 {
