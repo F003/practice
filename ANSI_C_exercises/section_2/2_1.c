d7131faa8ac35c2257ae20e1920bbb01db55ca0f
F003 <81417629+F003@users.noreply.github.com>
2022-08-16 20:48:32 +0200

    2.1 working probably not the safest program I've written but it works where it should ... Don't judge mi here :)

--- ANSI_C_exercises/section_2/2_1.c
+++ ANSI_C_exercises/section_2/2_1.c
@@ -3,34 +3,57 @@
 
 #define U_MIN 0 /* for every unsigned variable, the smallest value is 0 */
 
-int power(int base, int exponent); /* whole, positive exponentiation */
-int size(int min, int max); /* with given whole minimum and maximun value returns size in bits */
+unsigned long power(int base, int exponent); /* whole, positive exponentiation */
+int size(long min, unsigned long max); /* with given whole minimum and maximun value returns size in bits */
 int main()
 {
 	/* char */
-	printf("Char\nSize: %d bits\nMin: %d\nMax: %d\n\n", CHAR_BIT, CHAR_MIN, CHAR_MAX);
+	printf("\nChar\nSize: %d bits\nMin: %d\nMax: %d\n\n", CHAR_BIT, CHAR_MIN, CHAR_MAX);
 
 	/* unsigned char */
 	printf("Unsigned char\nSize: %d bits\nMin: %d\nMax: %d\n\n", size(U_MIN, UCHAR_MAX), U_MIN, UCHAR_MAX);
 
 	/* signed char */
-	printf("Signed char\nSize: %d bits\nMin: %d\nMax: %d\n\n", size(SCHAR_MAX, SCHAR_MIN), SCHAR_MIN, SCHAR_MAX);
+	printf("Signed char\nSize: %d bits\nMin: %d\nMax: %d\n\n", size(SCHAR_MIN, SCHAR_MAX), SCHAR_MIN, SCHAR_MAX);
+	
+	/* unsigned short */
+	printf("Unsigned short\nSize: %d bits\nMIN: %d\nMax: %d\n\n", size(U_MIN, USHRT_MAX), U_MIN, USHRT_MAX);
+
+	/* signed short */
+	printf("Signed short\nSize: %d bits\nMIN: %d\nMax: %d\n\n", size(SHRT_MIN, SHRT_MAX), SHRT_MIN, SHRT_MAX);
+	
+	/* unsigned int */
+	printf("Unsigned int\nSize: %d bits\nMIN: %d\nMax: %u\n\n", size(U_MIN, UINT_MAX), U_MIN, UINT_MAX);
+	
+	/* int */
+	printf("Signed int\nSize: %d bits\nMIN: %d\nMax: %d\n\n", size(INT_MIN, INT_MAX), INT_MIN, INT_MAX);
+	
+	/* unsigned long */
+	printf("Unsigned long\nSize: %d bits\nMIN: %d\nMax: %lu\n\n", size(U_MIN, ULONG_MAX), U_MIN, ULONG_MAX);
+
+	/* signed long */
+	printf("Signed long\nSize: %d bits\nMIN: %ld\nMax: %ld\n\n", size(LONG_MIN, LONG_MAX), LONG_MIN, LONG_MAX);
 
 	return 0;
 }
 
-int power(int b, int e)
+unsigned long power(int b, int e)
 {
-	int i;
+	unsigned long i;
 	for (i = 1; e > 0; --e) 
 		i = i * b;
 	return i;
 }
-/* not working yet */
-int size(int min, int max)
+
+int size(long min, unsigned long max)
 {
 	int i;
-	for (i = 0; power(2, i) <= max - min; ++i)
+	/* to make number of values storable in long they are devided by 4 before storing */
+	max = max/8;
+	min = min/8;
+	unsigned long nvalues_dev8 = max - min;
+	for (i = 0; power(2, i) <= nvalues_dev8; ++i)
 		;
+	i = i + 3; /* adjusting for nvalues being devided by 8 beforehand */
 	return i;
 }
