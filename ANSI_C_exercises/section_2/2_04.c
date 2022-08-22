6dcce0b46a9230b7d4944126e2ecca825454aa4a
F003 <81417629+F003@users.noreply.github.com>
2022-08-22 14:41:45 +0200

    2.4 working there was very interesting lesson in this exercise always think how exactly for works if you use the same value to indicate end of loop as the value you increment after your value will change and it is easy to make infinite loop break; is usefull sorry for my english :)

---
+++ ANSI_C_exercises/section_2/2_04.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define MAXLENGHT 1000
+#define DELETE "qwerty" //characters to delete
+
+int get_line(char line[], int maxlenght);
+void squeeze(char line[], char delete[]);
+
+int main()
+{
+	int c;
+	char line[MAXLENGHT], delete[MAXLENGHT] = DELETE;
+	while ((c = get_line(line, MAXLENGHT)) > 0) {
+		squeeze(line, delete);
+		printf("%s", line);
+	}
+	return 0;
+}
+
+int get_line(char line[], int max)
+{
+	int c, i;
+	for (i = 0; i < max - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		line[i] = c;
+	if (c == '\n')
+		line[i++] = c;
+	line[i] = '\0';
+	return i;
+}
+
+void squeeze(char line[], char del[])
+{
+	int i, j, k;
+
+	for (i = j = 0; line[i] != '\0'; ++i) {
+		for (k = 0; del[k] != '\0'; ++k) {
+			if (line[i] == del[k]) {
+				k = -1;
+				break;
+			}
+	}
+		if (k != -1) 
+			line[j++] = line[i];
+	}
+	line[j] = '\0';
+	return;
+}
